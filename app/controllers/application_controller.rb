fb9bba8b18811cee1899f209fec74defecf315b5
wicked <amelieoller@gmail.com>
2017-09-24 10:23:35 -0700

    Set up stylesheet, layout.erb and index.erb pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,12 @@ class ApplicationController < Sinatra::Base
     end
 
     get '/' do
-        "Hello World!"
+        erb :index
     end
 
     helpers do
         def current_user
-            @current_user ||= User.find_by(:id => session[:user_id] if session[:user_id])
+            @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
         end
 
         def logged_in?
