2a102f52cebe0e33de5f7a2f399bb371e3cd4cd5
wicked <amelieoller@gmail.com>
2017-09-24 11:18:20 -0700

    Add user login post controller and error message for wrong user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,20 @@ class UsersController < ApplicationController
     end
 
     get '/login' do
+        @message = session[:message]
         erb :'/users/login'
     end
+
+    post '/login' do
+        user = User.find_by(:name => params[:name])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/places"
+        else
+            session[:message] = "That didn't work... Try logging in again or sign up if you don't have an account yet."
+            redirect "/login"
+        end
+    end
+
 
 end
