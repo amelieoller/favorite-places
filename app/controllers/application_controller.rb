721f0f4a8aa69705b2bc3a0ee112a1aee4ba651f
wicked <amelieoller@gmail.com>
2017-09-24 10:16:20 -0700

    Add  and  helpers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,14 @@ class ApplicationController < Sinatra::Base
         "Hello World!"
     end
 
+    helpers do
+        def current_user
+            @current_user ||= User.find_by(:id => session[:user_id] if session[:user_id])
+        end
+
+        def logged_in?
+            !!current_user
+        end
+    end
+
 end
