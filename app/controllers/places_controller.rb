caf93fa61bda92251fe75ebb2283c5ece05a3a26
wicked <amelieoller@gmail.com>
2017-09-24 10:56:53 -0700

    Add controller get action for places show page and create show page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,4 +8,17 @@ class PlacesController < ApplicationController
         erb :'/places/new'
     end
 
+    post '/places' do
+        @place = current_user.places.new(params[:place])
+        @place.country = Country.find_or_create_by(params[:country])
+        @place.save
+
+        redirect "/places/#{@place.slug}"
+    end
+
+    get '/places/:slug' do
+        @place = Place.find_by_slug(params[:slug])
+        erb :'/places/show'
+    end
+
 end
