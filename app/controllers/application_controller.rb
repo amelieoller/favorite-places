98ea5adc0d5688f0d1e6c1d78c642cf2bc66b204
wicked <amelieoller@gmail.com>
2017-09-24 10:14:21 -0700

    Add  to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, 'password_security'
+    end
+
     get '/' do
         "Hello World!"
     end
