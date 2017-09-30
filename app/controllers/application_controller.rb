d93ec12e0780bd0e549eab9ea75a6318793e419c
wicked <amelieoller@gmail.com>
2017-09-30 11:25:15 -0700

    Add authenticate_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,4 +27,12 @@ class ApplicationController < Sinatra::Base
         end
     end
 
+    private 
+
+        def authenticate_user  
+            if !logged_in?
+                redirect "/login"
+            end
+        end
+
 end
