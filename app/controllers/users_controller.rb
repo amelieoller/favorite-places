62a9db94b01a4679c660bf1c9a3880a015d9cd6a
Amelie Oller <amelieoller@gmail.com>
2019-06-05 16:52:30 -0700

    Add duplicate user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,58 @@
 class UsersController < ApplicationController
 
-    get '/signup' do
-        @message = session.delete(:message)
-        erb :'/users/new'
-    end
+	get '/signup' do
+		@message = session.delete(:message)
+		erb :'/users/new'
+	end
 
-    post '/signup' do 
-        user = User.new(params)
-        if user.save
-            session[:user_id] = user.id
-            redirect "/places"
-        else
-            session[:message] = "Please fill out all of the fields and make sure your password is longer than 8 characters."
-            redirect "/signup"
-        end        
-    end
+	post '/signup' do 
+		user = User.find_by(name: params[:name])
 
-    get '/login' do
-        @message = session.delete(:message)
-        erb :'/users/login'
-    end
+		if user
+			session[:message] = "This username already exists, please choose a differnt one."
+			redirect "/signup"
+		else
+			user = User.new(params)
+			if user.save
+				session[:user_id] = user.id
+				redirect "/places"
+			else
+				session[:message] = "Please fill out all of the fields and make sure your password is longer than 8 characters."
+				redirect "/signup"
+			end
+		end
+	end
 
-    post '/login' do
-        user = User.find_by(:name => params[:name])
-        if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            redirect "/places"
-        else
-            session[:message] = "That didn't work... Try logging in again or sign up if you don't have an account yet."
-            redirect "/login"
-        end
-    end
+	get '/login' do
+		@message = session.delete(:message)
+		erb :'/users/login'
+	end
 
-    get '/logout' do
-        authenticate_user
-        session.destroy
-        redirect "/login"
+	post '/login' do
+		user = User.find_by(:name => params[:name])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/places"
+		else
+			session[:message] = "That didn't work... Try logging in again or sign up if you don't have an account yet."
+			redirect "/login"
+		end
+	end
 
-    end
+	get '/logout' do
+		authenticate_user
+		session.destroy
+		redirect "/login"
+	end
 
-    get '/users/:slug' do
-        authenticate_user
-        @user = User.find_by_slug(params[:slug])
-        if @user.id == current_user.id
-            erb :'/users/index'
-        else
-            redirect "/places/index"
-        end
-    end
+	get '/users/:slug' do
+		authenticate_user
+		@user = User.find_by_slug(params[:slug])
+		if @user.id == current_user.id
+			erb :'/users/index'
+		else
+			redirect "/places/index"
+		end
+	end
 
 end
